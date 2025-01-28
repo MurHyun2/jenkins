0b4a0790faece96be3e9285a0a8c430fd99fa686
이대현 <eddy1219@naver.com>
2025-01-29 01:10:17 +0900

    first

---
+++ jenkins-prj/src/main/java/org/example/jenkinsprj/JenkinsPrjApplication.java
@@ -0,0 +1,13 @@
+package org.example.jenkinsprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPrjApplication.class, args);
+    }
+
+}
